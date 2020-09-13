a898c5f46c254d7235c0943687a333caf61b163d
Michael <mlemay@oneweb.net>
2020-09-12 22:22:07 -0700

    placeholder page to test routiong

---
+++ src/pages/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <div class="center">
+        <h1>This is a second page to test routing.</h1>
+      </div>
+    </Layout>
+  )
+}
