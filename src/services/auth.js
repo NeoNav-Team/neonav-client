c1be6f7b6329ab835531ce81d1a71743fe3ea14b
Michael <mlemay@oneweb.net>
2020-09-13 16:35:41 -0700

    adds missing return

--- src/services/auth.js
+++ src/services/auth.js
@@ -20,12 +20,8 @@ export const userLogin = data => {
           console.log(error.response);
           return error.response;
         } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
           console.log(error.request);
         } else {
-          // Something happened in setting up the request that triggered an Error
           console.log('Error', error.message);
         }
         console.log(error.config);
