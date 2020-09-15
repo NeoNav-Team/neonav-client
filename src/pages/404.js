62909bd7d7bc11982a3bb8990582a49b2bb12af2
Michael <mlemay@oneweb.net>
2020-09-14 19:49:15 -0700

    updates 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,23 @@
-import React from "react";
-import Layout from "../components/layout"
+import React from 'react';
+import Layout from '../components/layout'
+import styled from 'styled-components';
+
+const BigText = styled.h1`
+  font-size: 30vh;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  color: #41C5FF;
+  transform: translate(-50%, -50%);
+  filter: drop-shadow(0 0 10px #41C5FF);
+`;
 
 export default function _404_() {
   return (
     <Layout>
-      <div>
-        <h1>404</h1>
-      </div>
+      <>
+        <BigText>404</BigText>
+      </>
     </Layout>
   )
 }
