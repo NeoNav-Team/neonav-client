47e28cf2f9382586813b972ff0fbc7d2b4141136
Michael <mlemay@oneweb.net>
2020-09-24 22:24:05 -0700

    second atempt to update live upload bug

--- src/components/modalEditAvatar.js
+++ src/components/modalEditAvatar.js
@@ -46,6 +46,12 @@ function ModalEditAvatar() {
         return isJpgOrPng && isLt2M;
     }
 
+    const handleAction  = ({ file, onSuccess }) => {
+        setTimeout(() => {
+          onSuccess("ok");
+        }, 0);
+      };
+
     const handleChange = info => {
         if (info.file.status === 'uploading') {
             setLoading(true);
@@ -99,7 +105,7 @@ function ModalEditAvatar() {
                         name="avatar"
                         listType="picture-card"
                         className="avatar-uploader"
-                        action={''}
+                        action={handleAction}
                         showUploadList={false}
                         beforeUpload={beforeUpload}
                         onChange={handleChange}
