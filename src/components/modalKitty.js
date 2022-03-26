de0a56887b645ffe60e00c37654f880af1e04c30
Michael LeMay <mjlemay@gmail.com>
2022-03-25 23:23:21 -0700

    fits kitty in her box

--- src/components/modalKitty.js
+++ src/components/modalKitty.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactPlayer from 'react-player';
+import { useWindowDimensions } from '../utils/responsive';
 
 function ModalKitty(props) {
+  const { width } = useWindowDimensions();
+  const maxWidth = width > 900 ? 900 : width - 10;
 
-  return <ReactPlayer url="https://sites.neonav.net/kitty.mp4" playing={true} controls={true} />         
+  return <ReactPlayer url="https://sites.neonav.net/kitty.mp4"  width={maxWidth} playing={true} controls={true} />         
 }
 export default ModalKitty;    
